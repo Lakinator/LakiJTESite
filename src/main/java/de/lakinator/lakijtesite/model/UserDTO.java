3eae277604c31de4ac8ff4214917c1faf194e236
Lakinator <SchalkLukas20@gmail.com>
2024-03-21 23:45:17 +0100

    Add 'admin' attribute to UserDTO and update layout
    The 'admin' attribute is added to UserDTO to acknowledge users with admin roles. The layout.jte file is updated to provide admin access based on the 'admin' attribute. This will allow admin users to have privileged access to certain parts of the application.

--- src/main/java/de/lakinator/lakijtesite/model/UserDTO.java
+++ src/main/java/de/lakinator/lakijtesite/model/UserDTO.java
@@ -1,9 +1,10 @@
 package de.lakinator.lakijtesite.model;
 
 import de.lakinator.lakijtesite.persistence.model.User;
+import de.lakinator.lakijtesite.security.UserRole;
 
-public record UserDTO( String username, String email ) {
+public record UserDTO( String username, String email, boolean admin ) {
     public UserDTO( User user ) {
-        this( user.getUsername(), user.getEmail() );
+        this( user.getUsername(), user.getEmail(), user.getRoles().contains( UserRole.ADMIN ) );
     }
 }
