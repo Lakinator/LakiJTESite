5007190173fea0dcb8ce7ec159efc3f1c9c9902e
Lakinator <SchalkLukas20@gmail.com>
2024-03-26 23:09:34 +0100

    Update dependencies and configurations for Eclipse Store
    This commit primarily updates the Eclipse Store version in the POM file, and replaces the dependency 'storage-embedded' with 'integrations-spring-boot3'. The old configuration file 'store-configuration.xml' has been deleted and its properties moved to 'application.properties'. Also, the outdated method of loading the Eclipse Store configuration in 'StorageConfig.java' has been removed.

--- src/main/java/de/lakinator/lakijtesite/persistence/config/StorageConfig.java
+++ src/main/java/de/lakinator/lakijtesite/persistence/config/StorageConfig.java
@@ -1,22 +1,12 @@
 package de.lakinator.lakijtesite.persistence.config;
 
 import de.lakinator.lakijtesite.persistence.model.DataRoot;
-import org.eclipse.store.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
-import org.eclipse.store.storage.embedded.configuration.types.EmbeddedStorageConfigurationBuilder;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageConfig {
-
-    @Bean
-    public EmbeddedStorageManager eclipseStorageManager() {
-        EmbeddedStorageConfigurationBuilder configurationBuilder = EmbeddedStorageConfiguration.load( "store-configuration.xml" );
-        return configurationBuilder.createEmbeddedStorageFoundation()
-                .createEmbeddedStorageManager()
-                .start();
-    }
 
     @Bean
     public DataRoot storageRoot( EmbeddedStorageManager storageManager ) {
